13be4a476c999b846bf3f0d7ddc2333b71e3625e
seoeunpark <s2019w29@e-mirim.hs.kr>
2020-11-03 01:05:51 +0900

    create story

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-function howToPlay() {
-    location.href="howToPlay.html"
+function story() {
+    location.href="story.html"
 }
 
 function start() {
     location.href="game1.html"
+}
+function HowToPlay(){
+    location.href="HowToPlay.html"
 }
