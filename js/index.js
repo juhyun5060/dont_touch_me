7b81dba4bf6ed964d49cb9a151abb19750b45413
Juhyun <s2019w09@e-mirim.hs.kr>
2020-11-13 14:33:11 +0900

    delete music

--- js/index.js
+++ js/index.js
@@ -5,13 +5,3 @@ function HowToPlay(){
 function start() {
     location.href="story.html"
 }
-
-function music() {
-    var bgm = new Audio('');
-    if(!bgm.canPlayType('audio/wav')) alert('브라우저가 wav 재생을 지원하지 않습니다.');
-    else {
-        var bgm_src="music/indexMusic.wav";
-        btm = new Audio(bgm_src)
-        bgm.play()
-    }
-}
